a42126417c4e754866d76e6619d74a07f031bbae
BrnCosta <brunooc.bc@gmail.com>
2023-10-22 01:19:37 -0300

    Refactoring methods

--- src/tdd/camelcase/CamelCase.java
+++ src/tdd/camelcase/CamelCase.java
@@ -4,10 +4,24 @@ import java.util.*;
 
 public class CamelCase {
     public static List<String> converterCamelCase(String original) {
-        return Arrays.asList(original);
+        List<String> result = new ArrayList<String>(Arrays.asList(""));
+        int wordIndex = 0;
+
+        for (char letter : original.toCharArray()) {
+            if (Character.isUpperCase(letter)) {
+                wordIndex++;
+                result.add("");
+                letter = Character.toLowerCase(letter);
+            }
+
+            String word = result.get(wordIndex) + letter;
+            result.set(wordIndex, word);
+        }
+
+        return result;
     }
 
     public static String getIndex(List<String> stringList, int index) {
-        return stringList.get(0);
+        return stringList.get(index);
     }
 }
